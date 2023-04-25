433785363209cd9da5fa56cc1330069f9bd192de
wangdi-linux <wangdi7670@163.com>
2023-04-25 16:13:57 +0800

    done proj2/relu.s

--- sp23-project/sp23-proj2-starter-main/src/relu.s
+++ sp23-project/sp23-proj2-starter-main/src/relu.s
@@ -12,27 +12,57 @@
 #   - If the length of the array is less than 1,
 #     this function terminates the program with error code 36
 # ==============================================================================
-relu:
-    # Prologue
 
+# void relu(int *arr, int length) {
+#     for (int i = 0; i < length; i++) {
+            ## if (arr[i] < 0) {
+            #     arr[i] = 0
+            # }
+#     }
+# }
+
+relu:
+    li t0 1
+    blt a1 t0 over
+
+    # Prologue
+    addi sp sp -12
+    sw s0 0(sp)
+    sw s1 4(sp)
+    sw s2 8(sp)
 
 loop_start:
-
-
-
-
-
+    addi s0 x0 0  # s0 = i
+    mv s1 a1  # s1 = length
+    mv s2 a0  # s2 = arr
 
 
 
 loop_continue:
+    beq s1 s0 loop_end
+    slli t0 s0 2  # t0 = i * 4
+    add t1 s2 t0  # t1 = arr + 4*i
 
-
+    lw t2 0(t1)  # t2 = arr[i]
+    blt t2 x0 if_label
+    j end_if  # jump to end_if
+    
+if_label:
+    sw x0 0(t1)
+end_if:
+    addi s0 s0 1  # i++
+    jal x0 loop_continue
 
 loop_end:
 
 
     # Epilogue
-
+    lw s0 0(sp)
+    lw s1 4(sp)
+    lw s2 8(sp)
+    addi sp sp 12
 
     jr ra
+over:
+    li a0 36
+    j exit
