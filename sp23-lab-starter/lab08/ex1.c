9b8d432a04b01f083fa2c716af9f1670b37725cf
wangdi-linux <wangdi7670@163.com>
2023-05-29 01:09:24 +0800

    done lab08/ex1

--- sp23-lab-starter/lab08/ex1.c
+++ sp23-lab-starter/lab08/ex1.c
@@ -12,6 +12,16 @@ double dotp_critical(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
     // Use the critical keyword here!
+    #pragma omp parallel for
+    for (int i = 0; i < arr_size; i++) {
+        double temp = x[i] * y[i];
+
+        #pragma omp critical
+        {
+            global_sum += temp;
+        }
+    }
+
     return global_sum;
 }
 
@@ -20,6 +30,12 @@ double dotp_reduction(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
     // Use the reduction keyword here!
+
+    #pragma omp parallel for reduction(+:global_sum)
+    for (int i = 0; i < arr_size; i++) {
+        global_sum += x[i] * y[i];
+    }
+
     return global_sum;
 }
 
@@ -28,5 +44,20 @@ double dotp_manual_reduction(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
     // Do NOT use the `reduction` directive here!
+    #pragma omp parallel
+    {
+        double local_sum= 0;
+
+        #pragma omp for
+        for (int i = 0; i < arr_size; i++) {
+            local_sum += x[i] * y[i];
+        }
+
+        #pragma omp critical
+        {
+            global_sum += local_sum;
+        }
+    }
+
     return global_sum;
 }
